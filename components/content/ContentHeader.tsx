7ceaab6e40102c751833936ebb3069c1b3743287
Winzlieb <winzlieb@c3d2.de>
2023-01-16 06:06:30 +0100

    header and footer elements copied from musiconn.performance

---
+++ components/content/ContentHeader.tsx
@@ -0,0 +1,87 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const ContentHeader: FunctionComponent<Props> = (props) =>
+    (
+        <div className="content-header">
+          <div className="container">
+            <nav className="breadcrumb">
+              <ol>
+                <li>
+                  <a href="https://performance.musiconn.de/" title="Startseite">
+                    Startseite
+                  </a>
+                </li>
+                <li>
+                  <a
+                      href="/exhibition"
+                      title="Veranstaltung"
+                  >
+                    Ausstellung
+                  </a>
+                </li>
+              </ol>
+            </nav>
+            <h1>Ausstellung</h1>
+            <button className="content-search-toggle" type="button">
+              <span className="sr-only">Suche öffnen</span>
+            </button>
+            <form
+                id="content-search-form"
+                action="https://performance.musiconn.de/event?tx_mpeext_eventplugin%5Bcontroller%5D=Event&cHash=6b0be78c0503ebc67a0933c0040800ab"
+                method="post"
+            >
+              <div>
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__referrer][@extension]"
+                    defaultValue=""
+                />
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__referrer][@controller]"
+                    defaultValue="Standard"
+                />
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__referrer][@action]"
+                    defaultValue="default"
+                />
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__referrer][arguments]"
+                    defaultValue="YTowOnt96c63755d291d785ea055b1adf8857dd04bcca44b"
+                />
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__referrer][@request]"
+                    defaultValue='{"@extension":null,"@controller":"Standard","@action":"default"}e6b68a6c0c2b6a316e9542a5f36fe1f9bf095a4f'
+                />
+                <input
+                    type="hidden"
+                    name="tx_mpeext_eventplugin[__trustedProperties]"
+                    defaultValue='{"quickSearch":1}44589eadd100880923713e59da018dd16f50ce22'
+                />
+              </div>
+              <label className="search-label" htmlFor="input-text">
+                Suche
+              </label>
+              <input
+                  placeholder="Suchwort"
+                  className="search-text"
+                  id="input-text"
+                  type="text"
+                  name="tx_mpeext_eventplugin[quickSearch]"
+              />
+              <button type="submit" id="search-button" className="search-button">
+                Suchen
+              </button>
+            </form>
+          </div>
+        </div>
+    );
+
+export default ContentHeader;
---
+++ components/layout/PerformanceHeader.tsx
@@ -0,0 +1,274 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const PerformanceHeader: FunctionComponent<Props> = (props) =>
+    (
+        <header className="page-header">
+          <div className="container">
+            {/* Header logo */}
+            <div className="brand">
+              <a
+                  className="logo"
+                  title="zur Homepage"
+                  href="https://performance.musiconn.de/"
+              >
+                musiconn.performance — Das Eingabe- und Recherchetool für die
+                Musikwissenschaft
+              </a>
+            </div>
+            {/* Main navigation */}
+            <nav className="main-navigation">
+              <ul className="primary-navigation">
+                <li className="submenu ">
+                  <a href="https://performance.musiconn.de/search" title="Recherche">
+                    Recherche
+                  </a>
+                  <ul>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/default-title-7/zur-suche-und-ihren-ergebnissen"
+                          title="Datengrundlage und Suche"
+                      >
+                        Datengrundlage und Suche
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="submenu ">
+                  <a
+                      href="https://performance.musiconn.de/projects"
+                      title="Alle Projekte"
+                  >
+                    Projekte
+                  </a>
+                  <ul>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/carl-philipp-emanuel-bachs-konzerte-in-hamburg"
+                          title="Carl Philipp Emanuel Bachs Konzerte in Hamburg"
+                      >
+                        Carl Philipp Emanuel Bachs Konzerte in Hamburg
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/dresdner-kreuzchor"
+                          title="Dresdner Kreuzchor"
+                      >
+                        Dresdner Kreuzchor
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/dresdner-philharmonie"
+                          title="Dresdner Philharmonie"
+                      >
+                        Dresdner Philharmonie
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/die-erstauffuehrungen-des-verdi-requiems-im-deutschsprachigen-raum"
+                          title="Die Erstaufführungen des 'Verdi-Requiems' im deutschsprachigen Raum"
+                      >
+                        Die Erstaufführungen des 'Verdi-Requiems' im deutschsprachigen
+                        Raum
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/fachinformationsdienst-musikwissenschaft"
+                          title="Fachinformationsdienst Musikwissenschaft"
+                      >
+                        Fachinformationsdienst Musikwissenschaft
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/fanny-hensels-sonntagsmusiken"
+                          title="Fanny Hensels Sonntagsmusiken"
+                      >
+                        Fanny Hensels Sonntagsmusiken
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/frankfurter-museumsgesellschaft"
+                          title="Frankfurter Museumsgesellschaft"
+                      >
+                        Frankfurter Museumsgesellschaft
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/internationalisierung-der-symphonik"
+                          title="Internationalisierung der Symphonik"
+                      >
+                        Internationalisierung der Symphonik
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/jenaer-konzerte-programme-und-plakate"
+                          title="Jenaer Konzerte - Programme und Plakate"
+                      >
+                        Jenaer Konzerte - Programme und Plakate
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/konzertprogramme-des-leipziger-konservatoriums-fuer-musik"
+                          title="Konzertprogramme des Leipziger Konservatoriums für Musik"
+                      >
+                        Konzertprogramme des Leipziger Konservatoriums für Musik
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/leipziger-synagogalchor"
+                          title="Leipziger Synagogalchor"
+                      >
+                        Leipziger Synagogalchor
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/lortzing-gesellschaft"
+                          title="Lortzing-Gesellschaft"
+                      >
+                        Lortzing-Gesellschaft
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/musica-migrans"
+                          title="Musica Migrans"
+                      >
+                        Musica Migrans
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/oper-in-berlin-1810-1830"
+                          title="Oper in Berlin 1810–1830"
+                      >
+                        Oper in Berlin 1810–1830
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/repertoireforschung-zum-leipziger-thomanerchor"
+                          title="Repertoireforschung zum Leipziger Thomanerchor"
+                      >
+                        Repertoireforschung zum Leipziger Thomanerchor
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/ritualdesign-fuer-die-ballettbuehne"
+                          title="Ritualdesign für die Ballettbühne"
+                      >
+                        Ritualdesign für die Ballettbühne
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/sammlung-bialik-hmtm-hannover"
+                          title="Sammlung Bialik (HMTM Hannover)"
+                      >
+                        Sammlung Bialik (HMTM Hannover)
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/sammlungen-der-slub-dresden"
+                          title="Sammlungen der SLUB Dresden"
+                      >
+                        Sammlungen der SLUB Dresden
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/concert-life-and-concert-venues-in-tokyo"
+                          title="Sinfonische Konzerte in Tokyo"
+                      >
+                        Sinfonische Konzerte in Tokyo
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/staatsoper-dresden"
+                          title="Staatsoper Dresden"
+                      >
+                        Staatsoper Dresden
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/projects/urauffuehrungen-der-ddr"
+                          title="Uraufführungen von Werken in und aus der DDR (1976-1990)"
+                      >
+                        Uraufführungen von Werken in und aus der DDR (1976-1990)
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a
+                      href="https://performance.musiconn.de/collections"
+                      title="Sammlungen"
+                  >
+                    Sammlungen
+                  </a>
+                </li>
+                <li>
+                  <a
+                      href="https://performance.musiconn.de/faq"
+                      title="Frequently asked questions"
+                  >
+                    FAQ
+                  </a>
+                </li>
+                <li>
+                  <a href="https://performance.musiconn.de/about" title="Über uns">
+                    Über uns
+                  </a>
+                </li>
+                <li className="submenu ">
+                  <a href="https://performance.musiconn.de/api" title="API">
+                    API
+                  </a>
+                  <ul>
+                    <li>
+                      <a
+                          href="https://performance.musiconn.de/api/autocomplete"
+                          title="autocomplete"
+                      >
+                        autocomplete
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            {/* Navigation toggle for mobile navigation */}
+            <button className="navigation-toggle" type="button">
+              <span className="sr-only">Menü öffnen</span>
+              <span className="icon-bar"/>
+              <span className="icon-bar"/>
+              <span className="icon-bar"/>
+            </button>
+          </div>
+          {/* Login toggle to activate overlay login modal */}
+          <div className="login-toggle">
+            <button className="login-toggle-button" type="button">
+              <span className="sr-only">Login anzeigen</span>
+            </button>
+          </div>
+        </header>
+    );
+
+export default PerformanceHeader;

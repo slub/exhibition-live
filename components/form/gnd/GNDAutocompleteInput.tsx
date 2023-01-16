c968ce48cff1b8fbce198c002ebc702b8eee1d54
Winzlieb <winzlieb@c3d2.de>
2023-01-16 06:10:44 +0100

    autocomplete component for NGD people search

---
+++ components/form/gnd/GNDAutocompleteInput.tsx
@@ -0,0 +1,32 @@
+import React, {FunctionComponent, useState} from 'react';
+import {AutocompleteSuggestion, DebouncedAutocomplete} from "../DebouncedAutoComplete";
+import findPersonWithinGND from "../../utils/gnd/findpersonWithinGND";
+
+interface OwnProps {
+}
+
+type Props = OwnProps;
+
+const buildLabelFromSuggestion: (suggestion: AutocompleteSuggestion) => string
+    = ({value, label}) => `${label}, GND: ${value}`
+
+const GNDAutocompleteInput: FunctionComponent<Props> = (props) => {
+  const [selected, setSelected] = useState<AutocompleteSuggestion | null>(null)
+
+  return (
+      <DebouncedAutocomplete
+          load={async (searchString) => searchString
+              ? (await findPersonWithinGND(searchString, 50)).map(({gndid, literal}) => ({
+                label: literal,
+                value: gndid
+              }))
+              : []}
+          placeholder="Search a person within the GND"
+          getOptionLabel={buildLabelFromSuggestion}
+          onChange={(_event: any, item: AutocompleteSuggestion | null) => {
+            setSelected(item)
+          }}/>
+  );
+};
+
+export default GNDAutocompleteInput;

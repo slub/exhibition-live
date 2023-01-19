1544fcfb0a7b9c41ac45498aa321fc57e6230adb
Winzlieb <winzlieb@c3d2.de>
2023-01-19 10:22:50 +0100

    add slub entity prefix to prefixes

--- components/exhibtion/prefixes.ts
+++ components/exhibtion/prefixes.ts
@@ -2,6 +2,7 @@ import {Prefixes} from '../utils/types'
 
 export const exhibitionPrefixes: Prefixes = {
   sladb: 'http://ontologies.slub-dresden.de/exhibiton#',
+  slent: 'http://ontologies.slub-dresden.de/exhibiton/entity#',
   slsrc: 'http://ontologies.slub-dresden.de/source#',
   slmeta: 'http://ontologies.slub-dresden.de/meta#',
   slperson: 'http://ontologies.slub-dresden.de/person#'

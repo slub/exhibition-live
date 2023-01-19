fd1a2270529153246155c1adbc41f84ef542be33
Winzlieb <winzlieb@c3d2.de>
2023-01-19 11:50:50 +0100

    generate random Entities from JSON Schema

--- components/form/EditExhibitionJSONForm.stories.tsx
+++ components/form/EditExhibitionJSONForm.stories.tsx
@@ -1,7 +1,9 @@
+import { button} from '@storybook/addon-knobs'
 import {ComponentMeta} from '@storybook/react'
+import { JSONSchemaFaker } from 'json-schema-faker'
 import {useState} from 'react'
 
-import EditExhibitionJSONForm from './EditExhibitionJSONForm'
+import EditExhibitionJSONForm, {exhibitionSchema} from './EditExhibitionJSONForm'
 
 export default {
   title: 'form/exhibition/EditExhibitionJSONForm',
@@ -9,6 +11,11 @@ export default {
 } as ComponentMeta<typeof EditExhibitionJSONForm>
 
 export const EditExhibitionJSOnFormDefault = () => {
-  const [data, setData] = useState<any>({})
+  const [data, setData] = useState<any>()
+  button('generate random entity state', () => {
+    // @ts-ignore
+    setData(JSONSchemaFaker.generate(exhibitionSchema))
+  })
+
   return <EditExhibitionJSONForm data={data} setData={setData}/>
 }

65e7c6936a7fd0063829cefa344a34db96fd15ca
Winzlieb <winzlieb@c3d2.de>
2023-01-19 10:26:14 +0100

    add AutoIdentifierRenderer, that gives an entity a random URI
    start JSONLD conversion of data generated from Form

--- components/form/EditExhibitionJSONForm.tsx
+++ components/form/EditExhibitionJSONForm.tsx
@@ -1,15 +1,19 @@
-import {JsonFormsCore, rankWith, schemaMatches} from '@jsonforms/core'
+import 'react-json-view-lite/dist/index.css'
+
+import {JsonFormsCore, rankWith, schemaMatches, scopeEndsWith} from '@jsonforms/core'
 import {materialCells, materialRenderers} from '@jsonforms/material-renderers'
 import {JsonForms} from '@jsonforms/react'
+import * as jsonld from 'jsonld'
 import isEmpty from 'lodash/isEmpty'
-import React, {FunctionComponent, useCallback} from 'react'
+import React, {FunctionComponent, useCallback, useState} from 'react'
+import {JsonView} from 'react-json-view-lite'
 
-import uischema from '../../schema/exhibition-form-ui-schema.json'
 import schema from '../../schema/exhibition-info.schema.json'
 import AutocompleteURIFieldRenderer from '../renderer/AutocompleteURIFieldRenderer'
+import AutoIdentifierRenderer from '../renderer/AutoIdentifierRenderer'
 import MaterialCustomAnyOfRenderer, {materialCustomAnyOfControlTester} from '../renderer/MaterialCustomAnyOfRenderer'
 
-const exhibitionSchema = { ...schema, ...schema.$defs.Exhibition}
+export const exhibitionSchema = {...schema, ...schema.$defs.Exhibition}
 
 interface OwnProps {
   data: any,
@@ -25,28 +29,58 @@ const renderers = [
     renderer: MaterialCustomAnyOfRenderer
   }, {
     tester: rankWith(10,
-    schemaMatches(
-        schema =>
-            Boolean(!isEmpty(schema) &&
-                schema.format?.startsWith('wikidata'))
-    )),
-    renderer: AutocompleteURIFieldRenderer}]
+        schemaMatches(
+            schema =>
+                Boolean(!isEmpty(schema) &&
+                    schema.format?.startsWith('wikidata'))
+        )),
+    renderer: AutocompleteURIFieldRenderer,
+  }, {
+    tester: rankWith(10,
+        scopeEndsWith('@id')
+    ),
+    renderer: AutoIdentifierRenderer
+  }
+]
+
+const sladb = 'http://ontology.slub-dresden.de/exhibition#'
+const slent = 'http://ontology.slub-dresden.de/exhibition/entities#'
 const EditExhibitionJSONForm: FunctionComponent<Props> = ({data, setData}) => {
+  const [jsonldData, setJsonldData] = useState<any>({})
   const handleFormChange = useCallback(
       (state: Pick<JsonFormsCore, 'data' | 'errors'>) => {
         setData(state.data)
-      }, [setData])
+        const jsonldDoc = {
+          '@context': {
+            '@vocab': sladb,
+            'xsd': 'http://www.w3.org/2001/XMLSchema#',
+            'birth_date': {
+              '@type': 'xsd:date'
+            },
+            'death_date': {
+              '@type': 'xsd:date'
+            }
+          },
+          ...JSON.parse(JSON.stringify(state.data))
+        }
+        jsonld.JsonLdProcessor.expand( jsonldDoc ).then(res => setJsonldData(res))
+        //jsonld.toRDF(jsonldDoc).then(res => console.log(res))
 
-  return (
-      <JsonForms
-          data={data}
-          renderers={renderers}
-          cells={materialCells}
-          onChange={handleFormChange}
-          schema={exhibitionSchema}
-          uischema={uischema}
-      />
+      }, [ setData, setJsonldData])
+
+
+  return (<>
+        <JsonForms
+            data={data}
+            renderers={renderers}
+            cells={materialCells}
+            onChange={handleFormChange}
+            schema={exhibitionSchema}
+        />
+        <JsonView data={jsonldData} shouldInitiallyExpand={(lvl) => lvl < 5}/>
+      </>
   )
 }
 
 export default EditExhibitionJSONForm
+
---
+++ components/renderer/AutoIdentifierRenderer.stories.tsx
@@ -0,0 +1,53 @@
+import {JsonFormsCore, rankWith, scopeEndsWith} from '@jsonforms/core'
+import {materialCells, materialRenderers} from '@jsonforms/material-renderers'
+import {JsonForms} from '@jsonforms/react'
+import {ComponentMeta} from '@storybook/react'
+import {useCallback, useState} from 'react'
+
+import AutoIdentifierRenderer from './AutoIdentifierRenderer'
+
+export default {
+  title: 'form/exhibition/AutoIdentifierRenderer',
+component: AutoIdentifierRenderer,
+} as ComponentMeta<typeof AutoIdentifierRenderer>
+
+const schema = {
+  '$schema': 'http://json-schema.org/draft-07/schema#',
+  '$id': 'https://example.com/person.schema.json',
+  'title': 'Person',
+  'description': 'A human being',
+  'type': 'object',
+  'properties': {
+    '@id': {
+      'title': 'http://ontologies.slub-dresden.de/exhibiton/entity#',
+      'type': 'string'
+    }
+  }
+}
+
+const renderers = [
+  ...materialRenderers,
+  {
+    tester: rankWith(10,
+        scopeEndsWith('@id')
+    ),
+    renderer: AutoIdentifierRenderer
+  }
+]
+export const AutoIdentifierRendererDefault = () => {
+  const [data, setData] = useState<any>({})
+
+  const handleFormChange = useCallback(
+      ({data}: Pick<JsonFormsCore, 'data' | 'errors'>) => {
+        setData(data)
+      }, [setData])
+
+  return <JsonForms
+      data={data}
+      renderers={renderers}
+      cells={materialCells}
+      onChange={handleFormChange}
+      schema={schema}/>
+}
+
+

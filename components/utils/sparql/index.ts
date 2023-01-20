aa84c8f26d5dcbb8632e0cfe77dc2b601d0c2065
Winzlieb <winzlieb@c3d2.de>
2023-01-20 18:38:58 +0100

    implement json schema to graph query features
    (will be part of graviola library)

--- components/utils/sparql/index.ts
+++ components/utils/sparql/index.ts
@@ -3,4 +3,6 @@ import {Prefixes} from '../types'
 export const prefixes2sparqlPrefixDeclaration =
     (prefixes: Prefixes) => Object.entries(prefixes).map(([k, v]) => `PREFIX ${k}: <${v}>`).join('\n')
 
+export * from './jsonSchema2construct'
+export * from './remoteSparqlQuery'
 export * from './selectQuery'

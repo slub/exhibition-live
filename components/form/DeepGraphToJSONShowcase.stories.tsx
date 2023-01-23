6e8e35d7010221c8515fd3e9edbd14ec12e8b830
Winzlieb <winzlieb@c3d2.de>
2023-01-23 07:17:01 +0100

    DeepGraph extraction showcase

---
+++ components/form/DeepGraphToJSONShowcase.stories.tsx
@@ -0,0 +1,22 @@
+import {boolean, number} from '@storybook/addon-knobs'
+import {ComponentMeta} from '@storybook/react'
+
+import DeepGraphToJSONShowcase from './DeepGraphToJSONShowcase'
+
+export default {
+  title: 'form/exhibition/DeepGraphToJSONShowcase',
+  component: DeepGraphToJSONShowcase
+} as ComponentMeta<typeof DeepGraphToJSONShowcase>
+
+export const DeepGraphToJSONShowcaseDefault = () => {
+
+  const omitEmptyArrays = boolean('omit empty arrays', true)
+  const omitEmptyObjects = boolean('omit empty objects',  true)
+  const maxRecursion = number('max recursion', 5, { min: 0, max: 20})
+  const maxRecursionEachRef = number('max rec each $ref', 2, { min: 0, max: 20 })
+  return <DeepGraphToJSONShowcase
+      omitEmptyArrays={omitEmptyArrays}
+      omitEmptyObjects={omitEmptyObjects}
+      maxRecursion={maxRecursion}
+      maxRecursionEachRef={maxRecursionEachRef}/>
+}

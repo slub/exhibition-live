028d985488f2e75c519d849644de255914a64993
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-03-02 09:49:19 +0100

    add initial nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for dev shell each default system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_latest
+            nodePackages_latest.yarn
+          ];
+        };
+      }
+    );
+}

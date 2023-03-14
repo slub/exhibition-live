0be74271788e862ec4f6dfae58dd3450af60c29a
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-03-14 07:21:05 +0000

    move ontology into public folder

--- components/exhibtion/useExhibitionData.ts
+++
@@ -1,5 +0,0 @@
-import {useRDFDataSources} from '../state/useRDFDataSources'
-
-export const useExhibitionData = () =>
-    useRDFDataSources(
-        ['aus.ttl', 'exhibition-info.owl2.ttl'].map(s => `http://localhost:4002/${s}`))

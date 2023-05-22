b494df25a84ffd73feee574edc9cf163d6305392
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-05-22 13:06:45 +0200

    return original schema if no schema stored in public folder

--- components/form/uischemaForType.ts
+++ components/form/uischemaForType.ts
@@ -6,11 +6,13 @@ import {BASE_IRI} from '../config'
 export const useUISchemaForType = (typeIRI: string) => {
   const typeName = useMemo(() => typeIRI.substring(BASE_IRI.length, typeIRI.length), [typeIRI])
   const {data} = useQuery(['uischema', typeIRI], () => fetch(`./uischema/${typeName}.uischema.json`).then(async res => {
-    const schema = await res.json()
+    let schema = null
+    try {
+       schema = await res.json()
+    } catch (e) {
+      console.log(`No uischema found for ${typeName}``)
+    }
     return schema
-  }).catch(() => {
-    //probably not found, silently fail
-    return null
   }), {
     retry: false,
     onError: (err) => {}

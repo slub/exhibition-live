93fd99189d30c2a9f1faed7134b30d6818b3f9a1
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-05-22 13:07:14 +0200

    more helper functions

--- components/utils/core/jsonSchema.ts
+++ components/utils/core/jsonSchema.ts
@@ -3,3 +3,9 @@ import {JSONSchema7, JSONSchema7Definition} from 'json-schema'
 export const isJSONSchema = (json: JSONSchema7Definition): json is JSONSchema7 => typeof json !== 'boolean'
 export const isJSONSchemaDefinition = (json: JSONSchema7Definition | JSONSchema7Definition[] | undefined): json is JSONSchema7Definition =>
     Boolean(json && !Array.isArray(json))
+
+export const filterPrimitiveProperties = (properties: JSONSchema7['properties']) => Object.fromEntries(
+    Object.entries(properties).filter(([, value]) => (value.type !== 'string' && value.type !== 'number' && value.type !== 'boolean')))
+
+export const filterExtendedProperties = (properties: JSONSchema7['properties']) => Object.fromEntries(
+    Object.entries(properties).filter(([, value]) => (value.type === 'string' || value.type === 'number' || value.type === 'boolean')))

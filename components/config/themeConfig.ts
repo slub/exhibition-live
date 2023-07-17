1f246e35dd19ae3d697c5ab8c208044d170189a1
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-07-17 08:37:09 +0200

    add custom theme

---
+++ components/config/themeConfig.ts
@@ -0,0 +1,38 @@
+// ** MUI Imports
+import { PaletteMode } from '@mui/material'
+
+// ** Types
+export type ContentWidth = 'full' | 'boxed'
+
+export type ThemeColor = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success'
+
+type ThemeConfig = {
+  mode: PaletteMode
+  templateName: string
+  routingLoader: boolean
+  disableRipple: boolean
+  navigationSize: number
+  menuTextTruncate: boolean
+  contentWidth: ContentWidth
+  responsiveFontSizes: boolean
+}
+
+const themeConfig: ThemeConfig = {
+  // ** Layout Configs
+  templateName: 'Materio' /* App Name */,
+  mode: 'light' /* light | dark */,
+  contentWidth: 'boxed' /* full | boxed */,
+
+  // ** Routing Configs
+  routingLoader: true /* true | false */,
+
+  // ** Navigation (Menu) Configs
+  menuTextTruncate: true /* true | false */,
+  navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
+
+  // ** Other Configs
+  responsiveFontSizes: true /* true | false */,
+  disableRipple: false /* true | false */
+}
+
+export default themeConfig

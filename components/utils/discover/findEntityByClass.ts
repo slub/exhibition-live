9c07e25b2ee2ac8e853cb2aa9cf118fa5ef73a00
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-07-18 07:47:18 +0200

    better entry search by using either title o name or description

--- components/utils/discover/findEntityByClass.ts
+++ components/utils/discover/findEntityByClass.ts
@@ -6,15 +6,31 @@ import {defaultQuerySelect} from '../sparql/remoteOxigraph'
 
 const findEntityByClass = async (searchString: string, typeIRI: string, doQuery: (query: string) => Promise<any>) => {
   const subjectV = variable('subject'),
-      nameV = variable('name')
-  let query = SELECT.DISTINCT` ${subjectV} ${nameV}`.WHERE`
-          ${subjectV} a <${typeIRI}> ;
-            :name ${nameV} . 
-            FILTER(contains(lcase(${nameV}), lcase("${searchString}") )) .
+      nameV = variable('name'),
+      titleV = variable('title'),
+      descriptionV = variable('description'),
+      concatenatedV = variable('concatenated'),
+      safeNameV = variable('safeName'),
+      safeTitleV = variable('safeTitle'),
+      safeDescriptionV = variable('safeDescription'),
+      oneOfTitleV = variable('oneOfTitle')
+  let query = SELECT.DISTINCT` ${subjectV} ${oneOfTitleV}`.WHERE`
+          ${subjectV} a <${typeIRI}> .
+            OPTIONAL {${subjectV} :name ${nameV} .}
+            OPTIONAL {${subjectV} :title ${titleV} .}
+            OPTIONAL {${subjectV} :description ${descriptionV} .}
+
+            BIND (COALESCE(${nameV}, "") AS ${safeNameV})
+            BIND (COALESCE(${titleV}, "") AS ${safeTitleV})
+            BIND (COALESCE(${descriptionV}, "") AS ${safeDescriptionV})
+    
+            BIND (CONCAT(${safeNameV}, " ", ${safeTitleV}, " ", ${safeDescriptionV}) AS ${concatenatedV})
+            BIND (COALESCE(${nameV}, ${titleV}, ${descriptionV}, "") AS ${oneOfTitleV})
+            FILTER(contains(lcase(${oneOfTitleV}), lcase("${searchString}") )) .
 `.LIMIT(10).build(defaultQueryBuilderOptions)
   query = `PREFIX : <${defaultPrefix}> ` + query
   const bindings = await doQuery(query)
-  return bindings.map((binding: any) => ({name: binding[nameV.value]?.value, value: binding[subjectV.value]?.value}))
+  return bindings.map((binding: any) => ({name: binding[oneOfTitleV.value]?.value, value: binding[subjectV.value]?.value}))
 
 }
 

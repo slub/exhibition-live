6d03608f4247d4b6d2be7a762b199746b16b295f
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-08-04 00:37:34 +0000

    use react query within CRUD provider

--- components/state/useSPARQL_CRUD.ts
+++ components/state/useSPARQL_CRUD.ts
@@ -8,10 +8,11 @@ import {useCallback, useEffect, useState} from 'react'
 
 import {jsonSchemaGraphInfuser, WalkerOptions} from '../utils/graph/jsonSchemaGraphInfuser'
 import {jsonSchema2construct} from '../utils/sparql'
+import {useQuery, useQueryClient} from '@tanstack/react-query'
 
 export interface SparqlBuildOptions {
-    base?: string;
-    prefixes?: Record<string, string | NamespaceBuilder>;
+  base?: string;
+  prefixes?: Record<string, string | NamespaceBuilder>;
 }
 
 export type CRUDFunctions = {
@@ -22,136 +23,161 @@ export type CRUDFunctions = {
 }
 
 export type CRUDOptions = CRUDFunctions & {
-    defaultPrefix: string
-    data: any
-    setData: (data: any) => void
-    walkerOptions?: Partial<WalkerOptions>
-    queryBuildOptions?: SparqlBuildOptions
-    upsertByDefault?: boolean
-    ready?: boolean
+  defaultPrefix: string
+  data: any
+  setData: (data: any) => void
+  walkerOptions?: Partial<WalkerOptions>
+  queryBuildOptions?: SparqlBuildOptions
+  upsertByDefault?: boolean
+  ready?: boolean,
+  refetchInterval?: number | false
 }
 
 export const useSPARQL_CRUD = (entityIRI: string | undefined, typeIRI: string | undefined, schema: JSONSchema7,
-                        {
-                            askFetch,
-                            constructFetch,
-                            defaultPrefix,
-                            updateFetch,
-                            setData,
-                            data,
-                            walkerOptions = {},
-                            queryBuildOptions,
-                            upsertByDefault
-                        }: CRUDOptions) => {
+                               {
+                                 askFetch,
+                                 constructFetch,
+                                 defaultPrefix,
+                                 updateFetch,
+                                 setData,
+                                 data,
+                                 walkerOptions = {},
+                                 queryBuildOptions,
+                                 upsertByDefault,
+                                   refetchInterval
+                               }: CRUDOptions) => {
 
-    const [isUpdate, setIsUpdate] = useState(false)
-    const [whereEntity, setWhereEntity] = useState<string | undefined>()
+  const [isUpdate, setIsUpdate] = useState(false)
+  const [whereEntity, setWhereEntity] = useState<string | undefined>()
+  const queryClient = useQueryClient()
 
-    useEffect(() => {
-        if (!entityIRI) return
-        const _whereEntity = ` <${entityIRI}> a <${typeIRI}> . `
-        setWhereEntity(_whereEntity)
-    }, [entityIRI, typeIRI, setWhereEntity])
+  useEffect(() => {
+    if (!entityIRI) return
+    const _whereEntity = ` <${entityIRI}> a <${typeIRI}> . `
+    setWhereEntity(_whereEntity)
+  }, [entityIRI, typeIRI, setWhereEntity])
 
-    const exists = useCallback(async () => {
-        if (!whereEntity) return false
-        const query = ASK`${whereEntity}`.build(queryBuildOptions)
-        try {
-            return await askFetch(query)
-        } catch (e) {
-            console.error(e)
-        }
-        return false
-    }, [whereEntity, setIsUpdate, defaultPrefix, askFetch])
+  const exists = useCallback(async () => {
+    if (!whereEntity) return false
+    const query = ASK`${whereEntity}`.build(queryBuildOptions)
+    try {
+      return await askFetch(query)
+    } catch (e) {
+      console.error(e)
+    }
+    return false
+  }, [whereEntity, setIsUpdate, defaultPrefix, askFetch])
 
-    const load = useCallback(
-        async () => {
-            if (!entityIRI || !whereEntity) return
-            const {
-                construct,
-                whereRequired,
-                whereOptionals
-            } = jsonSchema2construct(entityIRI, schema, [], ['@id', '@type'])
-            let query = CONSTRUCT`${construct}`.WHERE`
+  const load = useCallback(
+      async () => {
+        if (!entityIRI || !whereEntity) return
+        const {
+          construct,
+          whereRequired,
+          whereOptionals
+        } = jsonSchema2construct(entityIRI, schema, [], ['@id', '@type'])
+        let query = CONSTRUCT`${construct}`.WHERE`
                 ${whereEntity}
                 ${whereRequired}
                 ${whereOptionals}
                 `.build(queryBuildOptions)
-            query = `PREFIX : <${defaultPrefix}> ` + query
-            try {
-                const ds = await constructFetch(query)
-                const resultJSON = jsonSchemaGraphInfuser(defaultPrefix, entityIRI, ds as Dataset, schema, walkerOptions)
-                setIsUpdate(true)
-                setData(resultJSON)
-            } catch (e) {
-                console.error(e)
-            }
-        },
-        [entityIRI, whereEntity, setData, setIsUpdate, defaultPrefix, constructFetch]
-    )
+        query = `PREFIX : <${defaultPrefix}> ` + query
+        try {
+          const ds = await constructFetch(query)
+          const resultJSON = jsonSchemaGraphInfuser(defaultPrefix, entityIRI, ds as Dataset, schema, walkerOptions)
+          setIsUpdate(true)
+          //setData(resultJSON)
+          return resultJSON
+        } catch (e) {
+          console.error(e)
+        }
+      },
+      [entityIRI, whereEntity, setIsUpdate, defaultPrefix, constructFetch]
+  )
 
-    const remove = useCallback(
-        async () => {
-            if (!entityIRI || !whereEntity) return
-            const {
-                construct,
-                whereRequired,
-                whereOptionals
-            } = jsonSchema2construct(entityIRI, schema, ['@id'], ['@id', '@type'])
-            let query = DELETE` ${construct} `.WHERE`${whereEntity} ${whereRequired}\n${whereOptionals}`.build(queryBuildOptions)
+  const remove = useCallback(
+      async () => {
+        if (!entityIRI || !whereEntity) return
+        const {
+          construct,
+          whereRequired,
+          whereOptionals
+        } = jsonSchema2construct(entityIRI, schema, ['@id'], ['@id', '@type'])
+        let query = DELETE` ${construct} `.WHERE`${whereEntity} ${whereRequired}\n${whereOptionals}`.build(queryBuildOptions)
+        query = `PREFIX : <${defaultPrefix}> ` + query
+        await updateFetch(query)
+      },
+      [entityIRI, whereEntity, defaultPrefix, updateFetch],
+  )
+
+
+  const save = useCallback(
+      async () => {
+        if (!data || !entityIRI || !whereEntity) return
+        const _data = {
+          ...data,
+          '@id': entityIRI
+        }
+        const ntWriter = new N3.Writer({format: 'Turtle'})
+        const ds = await jsonld.toRDF(_data)
+
+        // @ts-ignore
+        const ntriples = ntWriter.quadsToString([...ds]).replaceAll('_:_:', '_:')
+
+        if (!isUpdate && !upsertByDefault) {
+          const updateQuery = INSERT.DATA` ${ntriples} `
+          const query = updateQuery.build()
+          await updateFetch(query)
+          setIsUpdate(true)
+        } else {
+          const {
+            construct,
+            whereRequired,
+            whereOptionals
+          } = jsonSchema2construct(entityIRI, schema, ['@id'], ['@id', '@type'])
+          const queries = [
+            DELETE` ${construct} `.WHERE`${whereEntity} ${whereRequired}\n${whereOptionals}`.build(queryBuildOptions),
+            INSERT.DATA` ${ntriples} `.build(queryBuildOptions)
+          ]
+          for (let query of queries) {
             query = `PREFIX : <${defaultPrefix}> ` + query
             await updateFetch(query)
-        },
-        [entityIRI, whereEntity, defaultPrefix, updateFetch],
-    )
-
-
-    const save = useCallback(
-        async () => {
-            if (!data || !entityIRI || !whereEntity) return
-          const _data = {
-            ...data,
-            '@id': entityIRI
           }
-            const ntWriter = new N3.Writer({format: 'Turtle'})
-            const ds = await jsonld.toRDF(_data)
+          await queryClient.invalidateQueries(['load', entityIRI])
+        }
+      },
+      [entityIRI, whereEntity, data, isUpdate, setIsUpdate, defaultPrefix, updateFetch, upsertByDefault, queryClient])
 
-            // @ts-ignore
-            const ntriples = ntWriter.quadsToString([...ds]).replaceAll('_:_:', '_:')
 
-            if (!isUpdate && !upsertByDefault) {
-                const updateQuery = INSERT.DATA` ${ntriples} `
-                const query = updateQuery.build()
-                await updateFetch(query)
-                setIsUpdate(true)
-            } else {
-                const {
-                    construct,
-                    whereRequired,
-                    whereOptionals
-                } = jsonSchema2construct(entityIRI, schema, ['@id'], ['@id', '@type'])
-                const queries = [
-                    DELETE` ${construct} `.WHERE`${whereEntity} ${whereRequired}\n${whereOptionals}`.build(queryBuildOptions),
-                    INSERT.DATA` ${ntriples} `.build(queryBuildOptions)
-                ]
-                for (let query of queries) {
-                    query = `PREFIX : <${defaultPrefix}> ` + query
-                    await updateFetch(query)
-                }
-            }
+  const {refetch} = useQuery(['load', entityIRI],
+      async () => {
+        const res = await load()
+        return res || null
+
+      }, {
+        onSuccess: (data) => {
+          if (data) {
+            console.log('onSuccess')
+            setData(data)
+          }
         },
-        [entityIRI, whereEntity, data, isUpdate, setIsUpdate, defaultPrefix, updateFetch, upsertByDefault])
+        enabled: Boolean(entityIRI && whereEntity),
+        refetchOnWindowFocus: false,
+        refetchInterval
+
+      })
+
 
   return {
-        exists,
-        load,
-        save,
-        remove,
-        isUpdate,
-        setIsUpdate,
-        // @ts-ignore
-        ready: Boolean(askFetch && constructFetch && updateFetch)
-    }
+    exists,
+    load: refetch,
+    save,
+    remove,
+    isUpdate,
+    setIsUpdate,
+    // @ts-ignore
+    ready: Boolean(askFetch && constructFetch && updateFetch)
+  }
 }
 
 

5cf73ca696998f4f0558369bb7bcb8acffee33a0
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-10-04 10:09:42 +0200

    add localization

---
+++ components/i18n/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import {initReactI18next} from 'react-i18next'
+
+i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+  fallbackLng:  'de',
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  }
+})
+
+export default i18n

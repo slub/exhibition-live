c0faa257e16542de00da50e2cd4a4c30656ff867
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-10-27 12:11:01 +0000

    add another story with Main Form

--- apps/exhibition-live/components/layout/main-layout/MainLayout.stories.tsx
+++ apps/exhibition-live/components/layout/main-layout/MainLayout.stories.tsx
@@ -1,7 +1,10 @@
-import {Meta} from "@storybook/react";
-import React from "react";
+import { Meta } from "@storybook/react";
+import React, { useMemo } from "react";
 
-import {MainLayout} from "./MainLayout";
+import { MainLayout } from "./MainLayout";
+import TypedForm from "../../content/main/TypedFormNoSSR";
+import { sladb, slent } from "../../form/formConfigs";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default {
   title: "layout/MainLayout",
@@ -9,3 +12,22 @@ export default {
 } as Meta<typeof MainLayout>;
 
 export const MainLayoutDefault = () => <MainLayout>content</MainLayout>;
+
+const typeName = "Exhibition",
+  classIRI: string | undefined = sladb(typeName).value,
+  defaultData = {
+    "@id": slent[`${typeName}#s-12`].value,
+    "@type": classIRI,
+  },
+  queryClient = new QueryClient();
+export const MainLayoutWithForm = () => (
+  <QueryClientProvider client={queryClient}>
+    <MainLayout>
+      <TypedForm
+        defaultData={defaultData}
+        typeName={typeName}
+        classIRI={classIRI}
+      />
+    </MainLayout>
+  </QueryClientProvider>
+);

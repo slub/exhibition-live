a1a7b347e54b9e34501025ae5adbd952d0300c42
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-11-02 10:33:14 +0000

    add some Form Stories for different Models

---
+++ apps/exhibition-live/components/form/Form.stories.tsx
@@ -0,0 +1,23 @@
+import TypedForm from "../content/main/TypedForm";
+import { sladb } from "./formConfigs";
+
+export default {
+  title: "forms/Form",
+  component: () => null,
+};
+
+export const TagForm = () => (
+  <div>
+    <TypedForm typeName={"Tag"} classIRI={sladb("Tag").value} />
+  </div>
+);
+export const LocationForm = () => (
+  <div>
+    <TypedForm typeName={"Location"} classIRI={sladb("Location").value} />
+  </div>
+);
+export const PlaceForm = () => (
+  <div>
+    <TypedForm typeName={"Place"} classIRI={sladb("Place").value} />
+  </div>
+);

f79735d75a99f7dedccd2aeca155413bccf12351
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-11-06 11:44:25 +0000

    remove unneeded datatype definitons in @context

--- apps/exhibition-live/components/form/formConfigs.ts
+++ apps/exhibition-live/components/form/formConfigs.ts
@@ -8,32 +8,8 @@ export const defaultPrefix = sladb[""].value;
 export const defaultJsonldContext = {
   "@vocab": defaultPrefix,
   xs: "http://www.w3.org/2001/XMLSchema#",
-  date: {
-    "@type": sladb.date.value,
-  },
-  birthDate: {
-    "@type": "xs:date",
-  },
-  deathDate: {
-    "@type": "xs:date",
-  },
   image: {
     "@type": "xs:anyURI",
-  },
-  fromDate: {
-    "@type": "xs:date",
-  },
-  toDate: {
-    "@type": "xs:date",
-  },
-  toType: {
-    "@type": "xs:integer",
-  },
-  fromType: {
-    "@type": "xs:integer",
-  },
-  lastNormUpdate: {
-    "@type": "xs:datetime",
   },
 };
 

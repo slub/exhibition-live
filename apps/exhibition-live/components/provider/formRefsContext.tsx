043fad1b265a4f2c7bef05dcc0954799219e576f
Astrid Probst <astrid.probst@slub-dresden.de>
2023-11-09 15:20:57 +0100

    add ref for drawer for similarityfinder

--- apps/exhibition-live/components/provider/formRefsContext.tsx
+++ apps/exhibition-live/components/provider/formRefsContext.tsx
@@ -9,11 +9,13 @@ type FormRefsContextValue = {
   stepperRef: RefObject<any | HTMLElement | undefined>;
   actionRef: RefObject<any | HTMLElement | undefined>;
   toolbarRef?: RefObject<any | HTMLElement | undefined>;
+  searchRef?: RefObject<any | HTMLElement | undefined>;
 };
 export const FormRefsContext = createContext<FormRefsContextValue>({
   stepperRef: undefined,
   actionRef: undefined,
   toolbarRef: undefined,
+  searchRef: undefined,
 });
 
 export const useFormRefsContext = () => React.useContext(FormRefsContext);
@@ -21,13 +23,14 @@ export const useFormRefsContext = () => React.useContext(FormRefsContext);
 const stepperRef = createRef<HTMLElement | undefined>();
 const actionRef = createRef<HTMLElement | undefined>();
 const toolbarRef = createRef<HTMLElement | undefined>();
+const searchRef = createRef<HTMLElement | undefined>();
 export const FormRefsProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
   return (
-    <FormRefsContext.Provider value={{ stepperRef, actionRef, toolbarRef }}>
+    <FormRefsContext.Provider value={{ stepperRef, actionRef, toolbarRef, searchRef }}>
       {children}
     </FormRefsContext.Provider>
   );

372c08f24d1a406a97bcbb36fd5c558d5a1c8430
Astrid Probst <astrid.probst@slub-dresden.de>
2023-11-09 10:20:20 +0100

    update Navigation list item

--- apps/exhibition-live/components/layout/main-layout/menu/NavCollapse.tsx
+++ apps/exhibition-live/components/layout/main-layout/menu/NavCollapse.tsx
@@ -105,7 +105,7 @@ export const NavCollapse = ({ menu, level }) => {
     <>
       <ListItemButton
         sx={{
-          borderRadius: `${customization.borderRadius}px`,
+          // borderRadius: `${customization.borderRadius}px`,
           mb: 0.5,
           alignItems: "flex-start",
           backgroundColor: level > 1 ? "transparent !important" : "inherit",

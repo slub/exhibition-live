2ab4bb002021d169ffe455b02152b31d1a4f73dc
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-11-21 01:27:06 +0000

    refactored remote provider state

--- apps/exhibition-live/components/state/useGlobalCRUDOptions.ts
+++ apps/exhibition-live/components/state/useGlobalCRUDOptions.ts
@@ -5,20 +5,53 @@ import { useCallback, useEffect, useState } from "react";
 
 import { oxigraphCrudOptions } from "../utils/sparql/remoteOxigraph";
 import { allegroCrudOptions } from "../utils/sparql/remoteAllegro";
-import { useSettings } from "./useLocalSettings";
+import {SparqlEndpoint, useSettings} from "./useLocalSettings";
 import { useOxigraph } from "./useOxigraph";
 import { CRUDFunctions } from "./useSPARQL_CRUD";
+import {qleverCrudOptions} from "../utils/sparql/remoteQlever";
 
 type UseGlobalCRUDOptions = () => {
   crudOptions?: CRUDFunctions;
   doLocalQuery: (query: string, mimeType?: string) => Promise<WorkerResult>;
 };
 
+type DoQuery = (query: string, mimeType?: string) => Promise<WorkerResult>
+const localWorkerCRUD = (_endpointConfig: SparqlEndpoint, options: { doQuery: DoQuery} ): CRUDFunctions => {
+  const doQuery = options.doQuery;
+  return ({
+    askFetch: async (query) => Boolean(await doQuery(query)),
+    // @ts-ignore
+    constructFetch: async (query) => {
+      const result = await doQuery(query, "application/turtle");
+      console.log({result});
+      if (!result?.data) return;
+      const reader = new N3.Parser();
+      return await datasetFactory.dataset(reader.parse(result.data));
+    },
+    // @ts-ignore
+    updateFetch: (query) => doQuery(query),
+    selectFetch: async (query, options) => {
+      const result = await doQuery(query);
+      return options?.withHeaders
+        ? result?.data
+        : result?.data?.results?.bindings;
+    },
+  });
+};
+
+const workerProvider: Record<SparqlEndpoint['provider'], <T = Record<string, any>>(endpointConfig: SparqlEndpoint, options?: T ) => CRUDFunctions > = {
+  oxigraph: oxigraphCrudOptions,
+  allegro: allegroCrudOptions,
+  worker: localWorkerCRUD as any,
+  qlever: qleverCrudOptions,
+  virtuoso: oxigraphCrudOptions,
+  blazegraph: oxigraphCrudOptions,
+}
+
+const getProviderOrDefault = (endpoint: SparqlEndpoint) => typeof workerProvider[endpoint.provider] === 'function' ? workerProvider[endpoint.provider] : oxigraphCrudOptions
+
 export const useGlobalCRUDOptions: UseGlobalCRUDOptions = () => {
   const { activeEndpoint } = useSettings();
-  const [localWorkerCRUDOptions, setLocalWorkerCRUDOptions] = useState<
-    CRUDFunctions | undefined
-  >();
   const [crudOptions, setCrudOptions] = useState<CRUDFunctions | undefined>();
   const { oxigraph, init } = useOxigraph();
   const doQuery = useCallback(
@@ -34,43 +67,17 @@ export const useGlobalCRUDOptions: UseGlobalCRUDOptions = () => {
 
   useEffect(() => {
     setCrudOptions(
-      activeEndpoint &&
-        (activeEndpoint.endpoint === "urn:worker"
-          ? localWorkerCRUDOptions
-          : activeEndpoint.auth?.token
-          ? allegroCrudOptions(activeEndpoint)
-          : oxigraphCrudOptions(activeEndpoint)),
+      activeEndpoint && (
+        getProviderOrDefault(activeEndpoint)(activeEndpoint,
+          activeEndpoint.provider === "worker" ? { doQuery,} : undefined
+        ))
     );
   }, [
-    localWorkerCRUDOptions,
+    doQuery,
     activeEndpoint?.endpoint,
     activeEndpoint?.auth,
     setCrudOptions,
   ]);
-
-  useEffect(() => {
-    const localWorkerCRUD: CRUDFunctions = {
-      askFetch: async (query) => Boolean(await doQuery(query)),
-      // @ts-ignore
-      constructFetch: async (query) => {
-        const result = await doQuery(query, "application/turtle");
-        console.log({ result });
-        if (!result?.data) return;
-        const reader = new N3.Parser();
-        return await datasetFactory.dataset(reader.parse(result.data));
-      },
-      // @ts-ignore
-      updateFetch: (query) => doQuery(query),
-      selectFetch: async (query, options) => {
-        const result = await doQuery(query);
-        return options?.withHeaders
-          ? result?.data
-          : result?.data?.results?.bindings;
-      },
-    };
-    setLocalWorkerCRUDOptions(localWorkerCRUD);
-  }, [doQuery, setLocalWorkerCRUDOptions]);
-
   return {
     crudOptions,
     doLocalQuery: doQuery,

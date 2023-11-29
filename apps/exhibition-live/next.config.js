425d8c466c2981fc71ae076667de1a4852731cb8
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-11-29 23:02:58 +0100

    temporary fix

--- apps/exhibition-live/next.config.js
+++ apps/exhibition-live/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/exhibition-live',
 }
 
 module.exports = nextConfig

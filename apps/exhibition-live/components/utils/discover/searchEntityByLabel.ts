1fae42c3b8aa58790a76594da180fa52d4418256
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2023-12-14 09:44:39 +0000

    helper function to search entity by exact label

---
+++ apps/exhibition-live/components/utils/discover/searchEntityByLabel.ts
@@ -0,0 +1,38 @@
+import { variable } from "@rdfjs/data-model";
+import { SELECT } from "@tpluscode/sparql-builder";
+import {
+  defaultPrefix,
+  defaultQueryBuilderOptions,
+} from "../../form/formConfigs";
+import { primaryFields } from "../../config";
+import { typeIRItoTypeName } from "../../content/main/Dashboard";
+
+export const searchEntityByLabel = async (
+  label: string,
+  typeIRI: string,
+  doQuery: (query: string) => Promise<any>,
+  limit: number = 10,
+) => {
+  const subjectV = variable("subject");
+  const typeName = typeIRItoTypeName(typeIRI);
+  const primaryField = primaryFields[typeName];
+  const labelField = primaryField?.label || "label";
+  const escapedLabel = label.replace(/"/g, '\\"');
+  let query = SELECT.DISTINCT` ${subjectV}`.WHERE`
+    ${subjectV} :${labelField} "${escapedLabel}" .
+    ${subjectV} a <${typeIRI}> .
+  `
+    .LIMIT(limit)
+    .build(defaultQueryBuilderOptions);
+
+  query = `PREFIX : <${defaultPrefix}>
+  ${query}`;
+
+  try {
+    const bindings = await doQuery(query);
+    return bindings.map((binding: any) => binding.subject.value);
+  } catch (e) {
+    console.error("Error finding entity by authority IRI", e);
+    return [];
+  }
+};

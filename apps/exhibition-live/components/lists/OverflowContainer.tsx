122eee52c6ec5f01f1904cc33c0a6bde837ffee8
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-01-06 08:14:27 +0000

    make overflow container accept custom tooltip

--- apps/exhibition-live/components/lists/OverflowContainer.tsx
+++ apps/exhibition-live/components/lists/OverflowContainer.tsx
@@ -1,21 +1,28 @@
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, useCallback } from "react";
 import { Tooltip, Typography } from "@mui/material";
 
 type OverflowContainerProps = {
   children: string;
+  tooltip?: string;
 };
-export const OverflowContainer = ({ children }: OverflowContainerProps) => {
+export const OverflowContainer = ({
+  children,
+  tooltip,
+}: OverflowContainerProps) => {
   const [tooltipEnabled, setTooltipEnabled] = useState(false);
 
-  const handleShouldShow = ({ currentTarget }: MouseEvent<Element>) => {
-    if (currentTarget.scrollWidth > currentTarget.clientWidth) {
-      setTooltipEnabled(true);
-    }
-  };
+  const handleShouldShow = useCallback(
+    ({ currentTarget }: MouseEvent<Element>) => {
+      if (tooltip || currentTarget.scrollWidth > currentTarget.clientWidth) {
+        setTooltipEnabled(true);
+      }
+    },
+    [setTooltipEnabled, tooltip],
+  );
 
   return (
     <Tooltip
-      title={children}
+      title={tooltip || children}
       open={tooltipEnabled}
       onClose={() => setTooltipEnabled(false)}
     >

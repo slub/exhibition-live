ef61cb2daba76edb4c5f1521a731ca8be22f61c2
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-01-09 21:39:05 +0000

    add PUBLIC_GAPI_OAUTH_CLIENT_ID to next config

--- apps/exhibition-live/next.config.js
+++ apps/exhibition-live/next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/exhibition-live',
   publicRuntimeConfig: {
+    NEXT_PUBLIC_GAPI_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GAPI_OAUTH_CLIENT_ID,
     NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '/exhibition-live',
   }
 }

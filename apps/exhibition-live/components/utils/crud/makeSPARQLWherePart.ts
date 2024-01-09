9d366a3824f24f6e89b733b7b973649124662358
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-01-09 21:01:39 +0000

    allow for multiple entities in where part builder

--- apps/exhibition-live/components/utils/crud/makeSPARQLWherePart.ts
+++ apps/exhibition-live/components/utils/crud/makeSPARQLWherePart.ts
@@ -1,14 +1,19 @@
 import { Variable } from "@rdfjs/types";
 
 export const makeSPARQLWherePart = (
-  entityIRI: string,
+  entityIRI: string | string[],
   typeIRI: string,
   subject: string | Variable = "?subject",
 ) => {
   const s = typeof subject === "string" ? subject : `?${subject.value}`;
+  const entityIRIList = Array.isArray(entityIRI) ? entityIRI : [entityIRI];
+  if (entityIRIList.length === 0) {
+    throw new Error("entityIRIList is empty, would result in invalid SPARQL");
+  }
+  const entityIRIValueString = `<${entityIRIList.join("> <")}>`;
   return typeIRI
-    ? ` ${s} a <${typeIRI}> . VALUES ${s} { <${entityIRI}> } `
-    : ` ${s} a ?type . VALUES ${s} { <${entityIRI}> } `;
+    ? ` ${s} a <${typeIRI}> . VALUES ${s} { ${entityIRIValueString} } `
+    : ` ${s} a ?type . VALUES ${s} { ${entityIRIValueString} } `;
 };
 
 export const withDefaultPrefix = (prefix: string, query: string) =>

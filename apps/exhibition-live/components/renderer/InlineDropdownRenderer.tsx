d640c9cc4a58904532730161da5710e9468c9239
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-02-02 13:00:36 +0000

    simplify dropdown renderer

--- apps/exhibition-live/components/renderer/InlineDropdownRenderer.tsx
+++ apps/exhibition-live/components/renderer/InlineDropdownRenderer.tsx
@@ -33,25 +33,19 @@ import { useGlobalCRUDOptions } from "../state/useGlobalCRUDOptions";
 const InlineDropdownRenderer = (props: ControlProps) => {
   const {
     id,
-    errors,
     schema,
     uischema,
     visible,
-    required,
-    renderers,
     config,
     data,
     handleChange,
     path,
     rootSchema,
     label,
-    description,
   } = props;
-  const [formData, setFormData] = useState<any>({ "@id": data });
   const appliedUiSchemaOptions = merge({}, config, uischema.options);
   const ctx = useJsonForms();
   const [realLabel, setRealLabel] = useState("");
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   const formsPath = useMemo(
     () => makeFormsPath(config?.formsPath, path),
     [config?.formsPath, path],
@@ -92,10 +86,9 @@ const InlineDropdownRenderer = (props: ControlProps) => {
         return;
       }
       if (v.value !== data) handleChange(path, v.value);
-      setFormData({ "@id": v.value });
       setRealLabel(v.label);
     },
-    [path, handleChange, data, setRealLabel, setFormData],
+    [path, handleChange, data, setRealLabel],
   );
 
   const handleOptionChange = useCallback(
@@ -130,92 +123,11 @@ const InlineDropdownRenderer = (props: ControlProps) => {
     [typeIRI],
   );
 
-  const router = useRouter();
-  const locale = router.query.locale || "";
-  const handleToggle = useCallback(
-    (event?: React.MouseEvent) => {
-      event?.stopPropagation();
-      //open new tab if middle mouse click
-      if (event?.button === 1) {
-        window.open(
-          `/${locale}/create/${typeName}?encID=${encodeIRI(data)}`,
-          "_blank",
-        );
-        return;
-      }
-      setModalIsOpen(!modalIsOpen);
-    },
-    [setModalIsOpen, modalIsOpen, locale, typeName],
-  );
-
-  const handleSaveAndClose = useCallback(() => {
-    setModalIsOpen(false);
-    const id = formData["@id"];
-    if (!id) return;
-    const fieldDecl = primaryFields[typeName] as PrimaryField | undefined;
-    let label = id;
-    if (fieldDecl?.label)
-      label = extractFieldIfString(formData, fieldDecl.label);
-    handleSelectedChange({
-      value: id,
-      label: typeof label === "string" ? label : id,
-    });
-  }, [setModalIsOpen, formData, handleSelectedChange]);
-
-  const handleClose = useCallback(() => {
-    setModalIsOpen(false);
-  }, [setModalIsOpen]);
-
-  const handleFormDataChange = useCallback(
-    (data: any) => {
-      setFormData(data);
-    },
-    [setFormData],
-  );
-
   const limit = useMemo(() => {
     return appliedUiSchemaOptions.limit || 100;
   }, [appliedUiSchemaOptions.limit]);
 
-  const newURI = useCallback(() => {
-    const prefix = schema.title || slent[""].value;
-    const iri = `${prefix}${uuidv4()}`;
-    const fieldDecl = primaryFields[typeName] as PrimaryField | undefined;
-    const labelKey = fieldDecl?.label || "title";
-    setFormData({ "@id": iri });
-    return iri;
-  }, [schema, data, setFormData]);
-
-  const handleAddNew = useCallback(
-    (event?: React.MouseEvent) => {
-      event?.stopPropagation();
-      newURI();
-      setModalIsOpen(true);
-    },
-    [setModalIsOpen, newURI],
-  );
-
   const { t } = useTranslation();
-
-  const handleAddNewWithinNewTab = useCallback(
-    (event: React.MouseEvent) => {
-      if (event.button !== 1) return;
-      event?.stopPropagation();
-      const newIRI = newURI();
-      handleSelectedChange({
-        value: newIRI,
-        label: `${t(typeName)} neu (${newIRI.substring(
-          newIRI.lastIndexOf("/") + 1,
-          newIRI.length,
-        )})`,
-      });
-      window.open(
-        `/${locale}/create/${typeName}?encID=${encodeIRI(newIRI)}`,
-        "_blank",
-      );
-    },
-    [setModalIsOpen, newURI, typeName, locale],
-  );
 
   const { crudOptions } = useGlobalCRUDOptions();
   const load = useCallback(
@@ -240,73 +152,22 @@ const InlineDropdownRenderer = (props: ControlProps) => {
 
   return (
     <Hidden xsUp={!visible}>
-      <Grid container alignItems="baseline">
-        <Grid
-          sx={{
-            transition: "all 0.3s ease-in-out",
-          }}
-          item
-          flex={"auto"}
-        >
-          <FormControl
-            fullWidth={!appliedUiSchemaOptions.trim}
-            id={id}
-            variant={"standard"}
-            sx={(theme) => ({ marginBottom: theme.spacing(2) })}
-          >
-            <PreloadedOptionSelect
-              title={label}
-              readOnly={Boolean(ctx.readonly)}
-              // @ts-ignore
-              load={load}
-              typeIRI={typeIRI}
-              value={selected}
-              onChange={handleOptionChange}
-            />
-          </FormControl>
-        </Grid>
-        {!ctx.readonly && (
-          <Grid item>
-            <Grid container direction="column" spacing={0}>
-              {typeof data == "string" && data.length > 0 && (
-                <Grid item>
-                  <IconButton
-                    sx={{ padding: 0 }}
-                    onClick={handleToggle}
-                    onAuxClick={handleToggle}
-                  >
-                    {modalIsOpen ? <EditOff /> : <Edit />}
-                  </IconButton>
-                </Grid>
-              )}
-              <Grid item>
-                <IconButton
-                  sx={{ padding: 0 }}
-                  onClick={handleAddNew}
-                  onAuxClick={handleAddNewWithinNewTab}
-                >
-                  {<Add />}
-                </IconButton>
-              </Grid>
-            </Grid>
-            {
-              <SemanticFormsModal
-                key={selected.value}
-                schema={subSchema as JsonSchema}
-                formData={formData}
-                entityIRI={formData["@id"]}
-                typeIRI={typeIRI}
-                label={label}
-                open={modalIsOpen}
-                askClose={handleSaveAndClose}
-                askCancel={handleClose}
-                onFormDataChange={handleFormDataChange}
-                formsPath={formsPath}
-              />
-            }
-          </Grid>
-        )}
-      </Grid>
+      <FormControl
+        fullWidth={!appliedUiSchemaOptions.trim}
+        id={id}
+        variant={"standard"}
+        sx={(theme) => ({ marginBottom: theme.spacing(2) })}
+      >
+        <PreloadedOptionSelect
+          title={label}
+          readOnly={Boolean(ctx.readonly)}
+          // @ts-ignore
+          load={load}
+          typeIRI={typeIRI}
+          value={selected}
+          onChange={handleOptionChange}
+        />
+      </FormControl>
     </Hidden>
   );
 };

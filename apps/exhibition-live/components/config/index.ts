812396fc7bc7fa9fb00659ad1c1aee8d710088f9
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-02-08 22:16:13 +0000

    env var to config

--- apps/exhibition-live/components/config/index.ts
+++ apps/exhibition-live/components/config/index.ts
@@ -1,6 +1,8 @@
 export const BASE_IRI = "http://ontologies.slub-dresden.de/exhibition#";
 export const API_URL = "http://sdvahndmgtest.slub-dresden.de:8000/graphql";
 
+export const PUBLIC_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 export * from "./lobidMappings";
 export * from "./primaryFields";
 export * from "./permissions";

63cd3d8cd6dbead9267638d4b70a77602632df16
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-03-19 16:52:21 +0100

    always open drawer by default (dirty)

--- apps/exhibition-live/components/state/useRightDrawerState.ts
+++ apps/exhibition-live/components/state/useRightDrawerState.ts
@@ -2,17 +2,19 @@ import { create } from "zustand";
 
 export type UseRightDrawerState = {
   open: boolean;
+  keepMounted: boolean;
   setOpen: (open: boolean | ((prev: boolean) => boolean)) => void;
   width: number;
   setWidth: (width: number | ((oldWidth: number) => number)) => void;
 };
 
 export const useRightDrawerState = create<UseRightDrawerState>((set, get) => ({
-  open: false,
-  setOpen: (open) =>
+  open: true,
+  keepMounted: true,
+  setOpen: (open) => get().keepMounted ? set({ open: true }) : (
     typeof open === "function"
       ? set({ open: open(get().open) })
-      : set({ open }),
+      : set({ open })),
   width: 500,
   setWidth: (widthPassed) => {
     const width = typeof widthPassed === "function" ? widthPassed(get().width) : widthPassed;

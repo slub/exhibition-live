a22a0248671f4c6d025b45c6291b6bd2a0051d7e
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-03-19 09:51:59 +0100

    add additional standalone config

---
+++ apps/exhibition-live/next.config.standalone.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/exhibition-live',
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_GAPI_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GAPI_OAUTH_CLIENT_ID,
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '/exhibition-live',
+  }
+}
+
+module.exports = nextConfig

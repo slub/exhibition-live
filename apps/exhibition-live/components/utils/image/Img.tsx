e9be8811a875ed3cb2f993d224cca61cb9c5fa6e
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-03-26 13:10:41 +0100

    log env and PUBLIC_BASE_PATH for debugging purposes

--- apps/exhibition-live/components/utils/image/Img.tsx
+++ apps/exhibition-live/components/utils/image/Img.tsx
@@ -1,7 +1,11 @@
 import Image, { ImageProps } from "next/image";
 import { PUBLIC_BASE_PATH } from "../../config";
+import { useEffect } from "react";
 
 export const Img = ({ src, alt, ...rest }: ImageProps) => {
+  useEffect(() => {
+    console.log({PUBLIC_BASE_PATH, env: process.env});
+  }, []);
   return (
     <Image
       src={`${PUBLIC_BASE_PATH}/${

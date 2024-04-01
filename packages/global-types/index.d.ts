dd860287f26fd75f9e2fe190223081aed26fe1f9
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-04-01 21:00:49 +0200

    provide a simple abstract store facade

--- packages/global-types/index.d.ts
+++ packages/global-types/index.d.ts
@@ -1,6 +1,7 @@
 import { SparqlEndpoint } from "@slub/edb-core-types";
 import { WalkerOptions } from "@slub/edb-graph-traversal";
 import { NamespaceBuilder } from "@rdfjs/namespace";
+import { JSONSchema7 } from "json-schema";
 
 export type EdbConfRaw = {
   BASE_IRI: string;
@@ -16,4 +17,36 @@ export type EdbConfRaw = {
 };
 export type Config = Omit<EdbConfRaw, "namespaceBase"> & {
   namespace: NamespaceBuilder<string>;
+};
+
+export type QueryType = {
+  search?: string;
+};
+
+export type DatastoreBaseConfig = {
+  schema: JSONSchema7;
+};
+
+export type InitDatastoreFunction<T extends DatastoreBaseConfig> = (
+  dataStoreConfig: T,
+) => AbstractDatastore;
+
+export type AbstractDatastore = {
+  loadDocument: (typeName: string, entityIRI: string) => Promise<any>;
+  upsertDocument: (
+    typeName: string,
+    entityIRI: string,
+    document: any,
+  ) => Promise<any>;
+  listDocuments: (
+    typeName: string,
+    limit?: number,
+    cb?: (document: any) => Promise<any>,
+  ) => Promise<any[]>;
+  findDocuments: (
+    typeName: string,
+    query: QueryType,
+    limit?: number,
+    cb?: (document: any) => Promise<any>,
+  ) => Promise<any[]>;
 };

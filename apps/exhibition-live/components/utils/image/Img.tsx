2ac3dce7c31a8cb0e4d1b8e4ff96171696375d4a
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-04-03 20:54:32 +0200

    remove logging

--- apps/exhibition-live/components/utils/image/Img.tsx
+++ apps/exhibition-live/components/utils/image/Img.tsx
@@ -2,17 +2,12 @@ import Image, { ImageProps } from "next/image";
 import { PUBLIC_BASE_PATH } from "../../config";
 import { useEffect } from "react";
 
-export const Img = ({ src, alt, ...rest }: ImageProps) => {
-  useEffect(() => {
-    console.log({ PUBLIC_BASE_PATH, env: process.env });
-  }, []);
-  return (
-    <Image
-      src={`${PUBLIC_BASE_PATH}/${
-        typeof src === "string" ? src : "Icons/no-image-placeholder.png"
-      }`}
-      alt={alt}
-      {...rest}
-    />
-  );
-};
+export const Img = ({ src, alt, ...rest }: ImageProps) => (
+  <Image
+    src={`${PUBLIC_BASE_PATH}/${
+      typeof src === "string" ? src : "Icons/no-image-placeholder.png"
+    }`}
+    alt={alt}
+    {...rest}
+  />
+);

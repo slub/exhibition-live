2e3c650100873465b251a9cbb4118129fee8ec12
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-04-09 12:39:37 +0200

    remove unneeded CSS and format layout.css properly

--- apps/exhibition-live/pages/_app.tsx
+++ apps/exhibition-live/pages/_app.tsx
@@ -1,11 +1,4 @@
-import "../styles/globals.css";
-import "../styles/jquery.typeahead.min.css";
-import "../styles/jquery-ui.min.css";
-import "../styles/highlight.min.css";
-import "../styles/tooltipster.bundle.min.css";
-import "../styles/tooltipster-sideTip-shadow.min.css";
 import "../styles/layout.css";
-import "../styles/temp.css";
 import "leaflet/dist/leaflet.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";

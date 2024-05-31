8338c708382b230239b5ac6588aefb116a1b6e84
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2024-05-31 16:32:15 +0200

    CI fix 2

--- packages/form-renderer/layout-renderer/src/MaterialCategorizationStepperLayoutWithPortal.tsx
+++ packages/form-renderer/layout-renderer/src/MaterialCategorizationStepperLayoutWithPortal.tsx
@@ -222,7 +222,7 @@ const withAjvProps =
   ) =>
   (props: P) => {
     const ctx = useJsonForms();
-    const ajv = getAjv({ jsonforms: { ...ctx } });
+    const ajv = getAjv({ jsonforms: { ...ctx } }) as AjvProps["ajv"];
 
     return <Component {...props} ajv={ajv} actionContainer={actionContainer} />;
   };

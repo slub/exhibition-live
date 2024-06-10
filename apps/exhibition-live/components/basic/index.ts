fccc0f3180c9fe1ffe1370bc9960e042a297170e
Winzlieb <winzlieb@c3d2.de>
2024-06-10 14:44:35 +0200

    cleanup and doc

--- apps/exhibition-live/components/basic/index.ts
+++ apps/exhibition-live/components/basic/index.ts
@@ -1,3 +1,2 @@
 export * from "./Link";
 export * from "./Img";
-export { useModalRegistry } from "@slub/edb-state-hooks";
--- apps/exhibition-live/components/form/jsonforms/index.ts
+++ apps/exhibition-live/components/form/jsonforms/index.ts
@@ -1,5 +1,13 @@
 import { JsonSchema, UISchemaElement } from "@jsonforms/core";
 
+/**
+ * Convert a JSON Schema to a list of UI Schema elements
+ * this is very usefull if you either do not have a specific UI Schema
+ * or if you want to generate a UI Schema from a JSON Schema and "fill the gaps"
+ * of elements that are not defined in the UI Schema
+ * @param jsonschema
+ * @param subpath
+ */
 export const jsonSchema2UISchemaElements: (
   jsonschema: JsonSchema,
   subpath?: string,

65a0b9e1bece51949224029b7a674658e1911431
Winzlieb <winzlieb@c3d2.de>
2024-07-24 11:42:29 +0200

    fix export script and alow for greter body size within nginx

--- docker/scripts/export-triples.sh
+++ docker/scripts/export-triples.sh
@@ -10,27 +10,35 @@ function help {
     echo "  -e endpoint-url  The url of the endpoint to query. Default: http://localhost:7878/query"
     echo "  -o output-file   The file to write the triples to. Default: dump.nt"
     echo "  -f format-mime   The mime type of the output file. Default: application/n-triples"
+    echo "  -d default-prefix   The default prefix to use in the query. Default: http://example.org/"
     echo "  -h               Show this help message"
     exit 0
 }
 
 #parse options
-while getopts "e:o:h" opt; do
+while getopts "e:o:f:d:h" opt; do
     case ${opt} in
         e ) endpoint_url=$OPTARG;;
         o ) output_file=$OPTARG;;
         f ) format_mime=$OPTARG;;
+        d ) default_prefix=$OPTARG;;
         h ) help;;
         \? ) help;;
     esac
 done
 
+prefixes=""
+
+if [ -n "$default_prefix" ]; then
+    prefixes="PREFIX : <$default_prefix>"
+fi
 
 curl "${endpoint_url}" \
-  -H 'Accept: ${format_mime},*/*;q=0.9' \
+  -H "Accept: ${format_mime},*/*;q=0.9" \
   -H 'Accept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7' \
   -H 'Connection: keep-alive' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  --data-raw 'query=CONSTRUCT%20%7B%20%3Fsub%20%3Fpred%20%3Fobj%20.%20%7D%20WHERE%20%7B%0A%20%20%3Fsub%20%3Fpred%20%3Fobj%20.%0A%7D' \
+  --verbose \
+  --data-urlencode "query=${prefixes} CONSTRUCT { ?sub ?pred ?obj . } WHERE { ?sub ?pred ?obj .}" \
   --compressed \
   --output ${output_file}

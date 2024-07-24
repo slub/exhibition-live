39408c290d39504d372d894f72764e153bedba5f
Winzlieb <winzlieb@c3d2.de>
2024-07-24 11:43:35 +0200

    externalize dependencies
    should fix bundling issues

--- packages/tsup-config/tsup.config.js
+++ packages/tsup-config/tsup.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
+  external: [
+    "react",
+    "react-dom",
+    "cross-fetch",
+    "@mui/material",
+    "@tanstack/react-query",
+  ], // Specify which modules are external
   splitting: false, // Set to true if you want code splitting in your ESM bundle
   sourcemap: true, // Generates source maps
   clean: true, // Cleans the outDir before building
